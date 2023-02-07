331d41c24d0b9e283ac47fdfb3e54439239dd3e8
Ильяс Юсупов <iu70980@gmail.com>
2023-02-07 23:15:24 +0300

    хз

--- WpfChat/MainWindow.xaml.cs
+++ WpfChat/MainWindow.xaml.cs
@@ -29,39 +29,6 @@ namespace WpfChat
             InitializeComponent();
             employeeList.ItemsSource = Mongo.FindAllEmployee(CurrentUser.currentUser.Login);
             // создаем подключение к хабу
-            connection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:7003/chat")
-                .Build();
-
-
-            // регистрируем функцию Receive для получения данных
-            //connection.On<string, string>("ReceiveMessage", (user, message) =>
-            //{
-            //    Dispatcher.Invoke(() =>
-            //    {
-            //        var newMessage = $"{user}: {message}";
-            //        chatbox.Items.Insert(0, newMessage);
-            //    });
-            //});
-            connection.On<string, string>("ReceiveMessage", (message, user) => {
-
-                // создаем элемент <b> для имени пользователя
-                Dispatcher.Invoke(() =>
-                {
-                    var newMessage = $"{user}: {message}";
-                    chatbox.Items.Insert(0, newMessage);
-                });
-            //    const userNameElem = document.createElement("b");
-            //    userNameElem.textContent = `${ user}: `;
-
-            //    // создает элемент <p> для сообщения пользователя
-            //    const elem = document.createElement("p");
-            //    elem.appendChild(userNameElem);
-            //    elem.appendChild(document.createTextNode(message));
-
-            //    const firstElem = document.getElementById("chatroom").firstChild;
-            //    document.getElementById("chatroom").insertBefore(elem, firstElem);
-            });
         }
 
         // обработчик нажатия на кнопку
@@ -83,7 +50,17 @@ namespace WpfChat
             receiver = (User)employeeList.SelectedItem;
             try
             {
-                // подключемся к хабу
+                connection = new HubConnectionBuilder()
+                .WithUrl("https://localhost:7003/chat")
+                .Build();
+                connection.On<string, string>("ReceiveMessage", (message, user) => {
+                    
+                    Dispatcher.Invoke(() =>
+                    {
+                        var newMessage = $"{user}: {message}";
+                        chatbox.Items.Insert(0, newMessage);
+                    });
+                });
                 await connection.StartAsync();
                 chatbox.Items.Add("Вы вошли в чат");
                 sendBtn.IsEnabled = true;
